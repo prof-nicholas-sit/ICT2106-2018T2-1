46681c246e2556a1ad875df0666b9c499f117799
Juun Tang <31942987+16SIC063U@users.noreply.github.com>
2018-03-29 18:27:53 +0800

    created AccessController and MetaData stub

---
+++ Prettyprinter/Controllers/AccessController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Prettyprinter.Models;
+
+namespace Prettyprinter.Controllers
+{
+    public class AccessController
+    {
+        public string _id { get; set; }
+        public string name { get; set; }
+        public string parentId { get; set; }
+        public string type { get; set; }
+        public string[] accessControl { get; set; }
+        public DateTime date { get; set; }
+
+        public List<MetaData> getMetaData(string path)
+        {
+            List<MetaData> result = new List<MetaData>();
+
+            result.Add(new MetaData("id1", "file1", "parentId1", "file", new string[] { "email1", "true", "true" }, new DateTime()));
+            result.Add(new MetaData("id1", "folder1", "parentId1", "folder", new string[] { "email2", "true", "true" }, new DateTime()));
+            result.Add(new MetaData("id1", "file2", "parentId1", "file", new string[] { "email3", "true", "true" }, new DateTime()));
+
+            return result;
+        }
+    }
+}
---
+++ Prettyprinter/Models/MetaData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prettyprinter.Models
+{
+    public class MetaData
+    {
+        public string _id { get; set; }
+        public string name { get; set; }
+        public string parentId { get; set; }
+        public string type { get; set; }
+        public string[] accessControl { get; set; }
+        public DateTime date { get; set; }
+
+        public MetaData(string _id, string name, string parentId, string type, string[] accessControl, DateTime date)
+        {
+            this._id = _id;
+            this.name = name;
+            this.parentId = parentId;
+            this.type = type;
+            this.accessControl = (string[])accessControl.Clone();
+            this.date = date;
+    }
+    }
+}
