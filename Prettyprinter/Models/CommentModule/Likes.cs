ffc24e8426c96ed4fbe35cbef70a276cb470c7ab
Teo Hwee Boon Teo <16SIC062S@SIT.Singaporetech.edu.sg>
2018-04-03 02:17:05 +0800

    test push class

---
+++ Prettyprinter/Models/CommentModule/Likes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prettyprinter.Models.CommentModule
+{
+    public class Likes
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+    }
+}
