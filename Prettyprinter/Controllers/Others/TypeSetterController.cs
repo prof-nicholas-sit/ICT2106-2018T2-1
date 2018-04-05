7f9905b2951fa505912515ddfeaf804900b8b3de
Juun Tang <31942987+16SIC063U@users.noreply.github.com>
2018-04-05 18:41:17 +0800

    Reorganising

---
+++ Prettyprinter/Controllers/Others/TypeSetterController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+
+
+namespace Prettyprinter.Controllers
+{
+    public class TypeSetterController
+    {
+        public FileController onCreate() {
+            FileController data = new FileController();
+            data.setContent(null);
+            data.setParentId("apath");
+            data.setNewFile(true);
+            return data;
+        }
+
+        public FileController editFile()
+        {
+            FileController data = new FileController();
+            data.setContent("asd");
+            data.setParentId("aPath");
+            data.setNewFile(true);
+            return data;
+        }
+
+    }
+}
