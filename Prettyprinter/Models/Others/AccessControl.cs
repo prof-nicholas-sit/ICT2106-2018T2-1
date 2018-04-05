f773d475fe4da37442b28e487a2256809f74c4fc
Juun Tang <31942987+16SIC063U@users.noreply.github.com>
2018-04-05 18:40:38 +0800

    Reorganise

---
+++ Prettyprinter/Models/Others/AccessControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Prettyprinter.Models
+{
+    public class AccessControl
+    {
+        [Key]
+        public string aID { get; set; }
+        public string _id { get; set; }
+        public string uID { get; set; }
+        public bool MODIFY { get; set; }
+        public bool COMMENT { get; set; }
+
+        public AccessControl(string aID, string _id, string uID, bool MODIFY, bool COMMENT)
+        {
+            this.aID = aID;
+            this._id = _id;
+            this.uID = uID;
+            this.MODIFY = MODIFY;
+            this.COMMENT = COMMENT;
+        }
+    }
+}
